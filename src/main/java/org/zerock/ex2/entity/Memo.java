db8ec135bb4f019649ede8def0dffdb38ce1ae65
HwaRyo <a98k98k@gmail.com>
2021-07-28 14:17:20 +0900

    21.07.28 spring_study

--- src/main/java/org/zerock/ex2/entity/Memo.java
+++ src/main/java/org/zerock/ex2/entity/Memo.java
@@ -1,14 +1,22 @@
 package org.zerock.ex2.entity;
 
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name= "tbl_memo")
 @ToString
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Memo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long mno;
+
+    @Column(length = 200, nullable = false)
+    private String memoText;
 }
